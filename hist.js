813162c72bad23ca13e36f6b38f801f70ec53a26
zoebaker <zoebaker31@gmail.com>
2021-05-20 15:45:21 -0700

    Update hist.js

--- hist.js
+++ hist.js
@@ -1,6 +1,6 @@
 
 
-var w = 1100;
+var w = 900;
 var h = 600;
 
 const margin = {top: 30, right: 20, bottom: 20, left: 30};
@@ -15,22 +15,21 @@ const g = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+//set up tooltip
 var div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0)
     .attr("transform", "translate(" + 300+ "," + 200 + ")");
 
-// var div = g.append("div")
-//     .attr("class", "tooltip")
-//     .style("opacity", 1)
-//     .attr('fill','blue')
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 var render = data => {
+
+    //set x and y values
     const xValue = d=>d.bin;
     const yValue = d=>d.avg;
 
+    //create x and y scales
     var xScale =d3.scaleBand()
         .domain(data.map(xValue))
         // .range([0,width/2]);
@@ -49,23 +48,11 @@ var render = data => {
         .attr("transform", "translate( "+ margin.left + "," + graphHeight + ")")
         .attr('id', 'xaxis');
 
-    // g.append('g').call(d3.axisLeft(yScale))
-    //     .attr("transform", "translate( "+ margin.left + "," + 0 + ")")
-
-    // g.append('g').call(yAxis)
-    //     .attr("transform", "translate( "+ margin.left + "," + 0 + ")")
-    //     .attr('id', 'yaxis')
-    //
-    // g.append('g').call(d3.axisBottom(xScale).ticks(width/40).tickSizeOuter(0))
-    //     .attr("transform", "translate( "+ margin.left + "," + graphHeight + ")")
-    //     .attr('id', 'xaxis');
-        // .attr("text-anchor", "end")
-
 // axes labels
 
     //x axis label
     g.append("text")
-        .attr("text-anchor", "midde")
+        .attr("text-anchor", "middle")
         .attr("x", width/2)
         .attr("y", height + 10)
         .text("Average Rides per Hour");
@@ -90,12 +77,12 @@ var render = data => {
         .attr('width', xScale.bandwidth())
         .attr('fill','steelblue')
         .attr('opacity',.7)
-        .attr('x', function(d){return xScale(xValue(d))+xScale.bandwidth()})
+        .attr('x', function(d){return xScale(xValue(d))+xScale.bandwidth()+10})
         .attr('y',function(d) { return yScale(yValue(d))})
         .attr('height',function(d) {return yScale(0) - yScale(yValue(d))})
-        .append('title').text(d=>d.avg)
         .attr('min', function(d){return d.x0})
         .attr('max',d=>d.x1)
+        .attr('stroke', 'white')
         .attr('opacity',.7)
         .on("mouseover", function(d) {
 
@@ -104,14 +91,14 @@ var render = data => {
 
             console.log('hello');
 
-            div.transition()
-                .duration(200)
-                .style("opacity", .9)
-                .attr('fill','orange');
-            div.innerHTML("Range:"+ here.getAttribute("min") + "-"+ here.getAttribute("max") + "<br/>"  + here.getAttribute("num"))
-            // tooltip.html("Jello")
-                .style("left", 30+ "px")
-                .style("top", (0) + "px");
+            // div.transition()
+            //     .duration(200)
+            //     .style("opacity", .9)
+            //     .attr('fill','orange');
+            // div.innerHTML("Range:"+ here.getAttribute("min") + "-"+ here.getAttribute("max") + "<br/>"  + here.getAttribute("num"))
+            // // tooltip.html("Jello")
+            //     .style("left", 30+ "px")
+            //     .style("top", (0) + "px");
         })
         .on("mouseout", function(d) {
 
@@ -128,42 +115,6 @@ var render = data => {
 
         });
 
-    var postpan = data.filter(function(d){return d.bin == 1 & d.period=='during' & d.direction == "origin"})
-
-    var test = document.getElementById('destination');
-    console.log(xScale(postpan[0].bin)+xScale.bandwidth())
-
-
-
-    var annotationBoxHeight = 40;
-
-
-    ////DIY Annotation Box///
-
-    // console.log(postpan[0])
-    // const duringOriginAnnotation = g.append('g')
-    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // g.append('line')
-    //     .style("stroke", "lightgreen")
-    //     .style("stroke-width", 1)
-    //     .attr("x1", xScale(postpan[0].bin)+(2*xScale.bandwidth()))
-    //     .attr("y1",200)
-    //     .attr("x2", xScale(postpan[0].bin)+xScale.bandwidth()+60)
-    //     .attr("y2", 150);
-
-    // g.append('rect')
-    //
-    //
-    //     var annotationBox = "rect"
-    //     .attr('x',xScale(postpan[0].bin)+xScale.bandwidth()+60)
-    //     .attr('y', 150+annotationBoxHeight/2)
-    //     .style('stroke', 'black')
-    //     .attr('fill', 'yellow')
-    //     .attr('width',60)
-    //     .attr('height',annotationBoxHeight )
-    //
-    //
 
     /////update functionality/////////
     d3.selectAll(".myRadio").on("change", update);
@@ -171,41 +122,34 @@ var render = data => {
 
     function update(){
         var choices = [];
+        //if radio button is clicked, add it to the array of clicked buttons
         d3.selectAll(".myRadio").each(function(d){
           radio=d3.select(this);
           if (radio.property("checked")){
             choices.push(radio.property("value"));
           }
         });
-        console.log(choices);
+
+        //if buttons are selected
         if (choices.length >0){
+            //if 2 selections are made, filter data for both selections
             if (choices.length == 2){
                  newData = data.filter(function(d){return d.direction == choices[0] &d.period == choices[1] })
-                // newData2 = data.filter(function(d){return d.period == choices[1] })
-                // newData= newData1.concat(newData2)
 
-           }
+           }//else return filtered data for the one selection
             else{
                 newData = data.filter(function(d){return d.direction == choices[0]|| d.period == choices[0]})
             }
-            console.log(newData);
-            // console.log(data)
-
-            // newData = data.filter(function(d,i){return choices.includes(d);});
 
 
-            // xScale =d3.scaleBand()
-            //     .domain(newData.map(xValue))
-            //     // .range([0,width/2]);
-            //     .range([0,width]);
-
+            //update yScale
             yScale = d3.scaleLinear()
                 .domain([0,d3.max(newData,yValue)])
                 // .range([0,graphHeight]);
                 .range([graphHeight,0]);
 
 
-
+            //remove old rectangles from chart
             g.selectAll("rect").remove();
 
             var boop = g.selectAll('rect')
@@ -214,53 +158,35 @@ var render = data => {
             boop.enter().append('rect')
                 .attr('width', xScale.bandwidth())
                 .attr('fill','steelblue')
+                .attr('stroke', 'white')
                 .attr('opacity',.7)
                 .attr('bin', d=>d.bin)
                 .merge(boop)
-                    // .attr('x', d => xScale(xValue(d)))
-                    .attr('x', function(d){return xScale(xValue(d))+(xScale.bandwidth()+4)})
-                // .attr('x', function(d){return xScale(xValue(d))+(1.5*xScale.bandwidth())})
+                    .attr('x', function(d){return xScale(xValue(d))+(xScale.bandwidth()+10)})
                     .attr('y',function(d) { return yScale(yValue(d))})
                     .attr('height',function(d) {return yScale(0) - yScale(yValue(d))})
+                    .attr('min', function(d){return d.x0})
+                    .attr('max',d=>d.x1)
                     .attr("num", function (d){return d.avg})
+
+                //mouseover functionality
                 .on("mouseover", function(d){
-                    console.log('hello')
-
-
                     var workingRect =  d3.select(this)
-
                     workingRect.
                     attr('fill','orange');
 
-
-
-                    console.log(workingRect.attr('y')-workingRect.attr('height'))
-
-
-
+                    //update div
                     div.transition()
                         .duration(200)
                         .style("opacity", .9)
                         .attr('fill','orange');
-                    // div.innerHTML("Range:"+ here.getAttribute("min") + "-"+ here.getAttribute("max") + "<br/>"  + here.getAttribute("num"))
-                    div.html(workingRect.attr("num")+" stations had an average of "+workingRect.attr("bin")+" Rides per hour")
-                        // .style("left", (workingRect.attr('x'))+"px")
+
+                    div.html(workingRect.attr("num")+" station(s) had an average of "+workingRect.attr("min")+"-"+workingRect.attr("max")+" rider(s) per hour")
                         .style('left',500+"px")
                         .style("top",  (450)+"px")
 
-                        // tooltip.html("Jello")
-                        // .style("left", 30+ "px")
-                        // .style("top", (0) + "px");
-
-                console.log(d3.select(this).attr("bin"))
-
-                // div.transition()
-                //     .duration(200)
-                //     .style("opacity", .9);
-                // div.html(d3.select(this).min)
-                //     .style("left", 500+ "px")
-                //     .style("top", (500) + "px");
             })
+                //mouseout functionality
                 .on("mouseout", function(d) {
                     d3.select(this).
                     attr('fill','steelblue');
@@ -268,108 +194,31 @@ var render = data => {
                   thisrect = d3.select(this)._groups[0][0];
                   console.log(thisrect)
                   console.log(thisrect.getAttribute("num"))
+
                     div.transition()
-                        .duration(200)
+                        .duration(800)
                         .style("opacity", 0);
-                    // div.html(thisrect.min)
-                    //     .style("left", 500+ "px")
-                    //     .style("top", (500) + "px");
 
-                })
-            .append('title').text(d=>d.avg);
-
-            boop.exit().remove()
-
-
-            //
-            // //create new axes
+                });
 
 
 
-            // g.append('g').call(d3.axisLeft(yScale))
-            //     .attr("transform", "translate( "+ margin.left + "," + 0 + ")")
-            //
-            // g.append('g').call(d3.axisBottom(xScale).ticks(width/40).tickSizeOuter(0))
-            //     .attr("transform", "translate( "+ margin.left + "," + graphHeight + ")");
-
-            // var axisTest = g.selectAll("g.y.axis")
-            //  console.log(axisTest)
-            //
-
+           //update the y axis
             yAxis.remove();
-            // xAxis.remove();
-
             yAxis = g.append('g').call(d3.axisLeft(yScale))
                 .attr("transform", "translate( "+ margin.left + "," + 0 + ")")
 
-            // xAxis = g.append('g').call(d3.axisBottom(xScale).ticks(width/40).tickSizeOuter(0))
-            //     .attr("transform", "translate( "+ margin.left + "," + graphHeight + ")");
-
-
-            //  g.append('g').call(d3.axisLeft(yAxis))
-            //      .attr("transform", "translate( "+ margin.left + "," + 0 + ")")
-            //
 
 
 
     }}
 
 
-
-
-    // var preButton = document.getElementById('pre');
-    // console.log(preButton)
-    //
-    // preButton.onclick = function() {
-    //     var filtered = data.filter(function (d) {return d.period == 'before';});
-    //     console.log(filtered);
-    //     update1(filtered);
-    // }
-    //
-    // var duringButton = document.getElementById('during');
-    // duringButton.onclick = function() {
-    //     var filtered = data.filter(function (d) { return d.period == 'during';});
-    //     console.log(filtered)
-    //     update1(filtered);
-    // }
-    //
-    // var afterButton = document.getElementById('post');
-    // afterButton.onclick = function() {
-    //     var filtered = data.filter(function (d) { return d.period == 'post';});
-    //     console.log(filtered)
-    //     update1(filtered);
-    // }
-    //
-    //
-    // var originButton = document.getElementById('origin');
-    // originButton.onclick = function() {
-    //     var filtered = data.filter(function (d) { return d.direction == 'origin';});
-    //     console.log(filtered)
-    //     update(filtered);
-    // }
-    //
-    // var destinationButton = document.getElementById('destination');
-    // destinationButton.onclick = function() {
-    //     var filtered = data.filter(function (d) { return d.direction == 'destination';});
-    //     console.log(filtered)
-    //     update(filtered);
-    // }
-
-    g.append('text')
-        .attr("class", "caption")
-        .attr("font-family", "'Work sans', sans-serif")
-        .attr("font-size", 30)
-        .attr("x", w/2)
-        .attr("y", h-2)
-        .style('text-anchor', 'middle')
-        .html('Histogram of BART Stations Binned by Average Rides Per Hour')
-
-
 }
 
 
 
-d3.csv("bartHistData.csv").then(data => {
+d3.csv("histData.csv").then(data => {
     data.forEach(d => {
         d.bin = d["Bin"];
         d.period = d['Period'];
